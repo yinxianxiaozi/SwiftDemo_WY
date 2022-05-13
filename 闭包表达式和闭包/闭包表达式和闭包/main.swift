98ad971391bd0565b91fff11cc9b42edef104b81
wenyizzhang <wenyizzhang@tencent.com>
2022-05-13 22:27:08 +0800

    闭包表示式增加代码

--- 闭包表达式和闭包/闭包表达式和闭包/main.swift
+++ 闭包表达式和闭包/闭包表达式和闭包/main.swift
@@ -8,25 +8,78 @@
 import Foundation
 
 print("Hello, World!")
+func test1() {
+    /*
+     1、闭包表达式的写法
+     */
+    //1.1 函数
+    func sum(_ v1: Int, _ v2: Int) -> Int {
+        v1 + v2
+    }
+    print("sum\(sum(10, 20))")
+
+    ////1.2 闭包表达式
+//    var fn = {
+//        (v1: Int,v2: Int) -> Int in
+//        return v1 + v2
+//    }
+//
+//    let result = fn(10,20)
+//
+//    //1.3 匿名闭包表达式
+//    {
+//       (v1: Int,v2: Int) -> Int in
+//       return v1 + v2
+//    }(10,20)
+}
+
+test1()
 
 /*
- 1、闭包表达式的写法
+ 2、闭包表达式的简写
  */
-//函数
-func sum(_ v1: Int, _ v2: Int) -> Int {
-    v1 + v2
-}
-print("sum\(sum(10, 20))")
-
-//闭包表达式
-var fn = {
-    (v1: Int,v2: Int) -> Int in
+func test2 (){
+    //2.1 函数
+    func exec(v1: Int, v2: Int, fn: (Int, Int) -> Int) {
+        print(fn(v1, v2))
+    }
+    //2.2 闭包表达式正常格式
+    exec(v1: 10, v2: 20, fn: {
+    (v1: Int, v2: Int) -> Int in
     return v1 + v2
-}
-var result = fn(10, 20)
+    })
 
-//匿名闭包表达式
-{
-   (v1: Int,v2: Int) -> Int in
-   return v1 + v2
-}(10,20)
+    //2.3 简写：参数类型
+    exec(v1: 10, v2: 20, fn: {
+    v1, v2 in return v1 + v2
+    })
+
+    //2.4 简写：return
+    exec(v1: 10, v2: 20, fn: {
+    v1, v2 in v1 + v2
+    })
+
+    //2.5 简写：参数
+    exec(v1: 10, v2: 20, fn: { $0 + $1 })
+
+    //2.6 简写：只写操作符
+    exec(v1: 10, v2: 20, fn: + )
+}
+test2()
+
+/*
+ 3、尾随闭包
+ */
+func test3 () {
+    //3.1 函数定义
+    func exec1(v1: Int, v2: Int, fn: (Int, Int) -> Int) {
+        print(fn(v1, v2))
+    })
+    
+    //3.2 函数调用时尾随闭包
+    exec(v1: 10, v2: 20) { $0 + $1 }
+}
+
+
+
+
